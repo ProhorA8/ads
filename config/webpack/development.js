ffd1330cfca31651c2fb0917f81493dd6ca74e4b
ProhorA8 <Procustom4@gmail.com>
2021-12-20 10:01:35 +0300

    fix webpacker settings

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+// ERROR in chunk application [entry]
+const config = environment.toWebpackConfig();
+config.output.filename = "js/[name]-[hash].js"
+//
+
 module.exports = environment.toWebpackConfig()
