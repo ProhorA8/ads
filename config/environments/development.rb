c5eb63148f908d1a3d9494ad770fedc8b421f37f
ProhorA8 <Procustom4@gmail.com>
2022-01-05 15:18:11 +0300

    send email dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,10 +33,18 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    user_name: ENV['SEND_EMAIL'], # не используйте для тестов свои реальные ящики
+    password: ENV['SEND_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
