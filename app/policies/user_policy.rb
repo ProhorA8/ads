aa5259a4b1766d1f55b16987a9f89ede1533b836
ProhorA8 <Procustom4@gmail.com>
2022-01-27 16:50:56 +0300

    adding policy to user

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,12 +1,20 @@
 class UserPolicy < ApplicationPolicy
-  # Смотреть юзера может только он сам или админ
+  # Смотреть юзера может, если юзер присутствует, он сам или админ
   def show?
-    user.present? && (user == record || user.role == 'admin' || user.role == 'user')
+    user.present? && (user == record || admin?(record) || user?(record))
   end
 
-  class Scope < Scope
-    def resolve
-      scope
-    end
+  def admin_forbidden?
+    !admin?(record)
+  end
+
+  private
+
+  def admin?(user)
+    user.role == 'admin'
+  end
+
+  def user?(user)
+    user.role == 'user'
   end
 end
